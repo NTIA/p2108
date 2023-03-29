bc9c7f065c75a74924f23de9bfb488ed3aada054
Billy Kozma <wkozma@ntia.doc.gov>
2023-03-29 12:19:40 -0600

    mixed up percentages and probabilities

--- src/InverseComplementaryCumulativeDistribution.cpp
+++ src/InverseComplementaryCumulativeDistribution.cpp
@@ -24,7 +24,7 @@ double InverseComplementaryCumulativeDistribution(double q)
     double D_3 = 0.001308;
 
     double x = q;
-    if (q > 50)
+    if (q > 0.5)
         x = 1.0 - x;
 
     double T_x = sqrt(-2.0 * log(x));
@@ -33,7 +33,7 @@ double InverseComplementaryCumulativeDistribution(double q)
 
     double Q_q = T_x - zeta_x;
 
-    if (q > 50)
+    if (q > 0.5)
         Q_q = -Q_q;
 
     return Q_q;
