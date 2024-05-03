df510f993a50940c21155d480b59d409ecd137ed
Anthony Romaniello <aromaniello@ntia.gov>
2024-05-03 12:07:04 -0400

    Cross-platform change to DLLEXPORT

--- include/ITS.ITU.PSeries.P2108/P2108.h
+++ include/ITS.ITU.PSeries.P2108/P2108.h
@@ -1,8 +1,18 @@
 #ifndef __ITS_ITU_PSERIES_P2108__
 #define __ITS_ITU_PSERIES_P2108__
 
-#include <cmath>  // For atan, fmin, log10, pow, sqrt, tan,
-#define DLLEXPORT extern "C" __declspec(dllexport)
+#include <cmath>  // For atan, fmin, log10, pow, sqrt, tan
+
+// Define cross-platform EXPORTED
+#ifdef _WIN32
+#   ifdef P2108_EXPORTS
+#       define EXPORTED extern "C" __declspec(dllexport)
+#   else
+#       define EXPORTED extern "C" __declspec(dllimport)
+#   endif
+#else
+#   define EXPORTED
+#endif
 
 #define PI      3.1415926535897932384
 
@@ -20,11 +30,11 @@ using std::tan;
 //
 
 // Public Functions
-DLLEXPORT int AeronauticalStatisticalModel(double f__ghz, double theta__deg, 
+EXPORTED int AeronauticalStatisticalModel(double f__ghz, double theta__deg, 
     double p, double* L_ces__db);
-DLLEXPORT int TerrestrialStatisticalModel(double f__ghz, double d__km, 
+EXPORTED int TerrestrialStatisticalModel(double f__ghz, double d__km, 
     double p, double* L_ctt__db);
-DLLEXPORT int HeightGainTerminalCorrectionModel(double f__ghz, double h__meter,
+EXPORTED int HeightGainTerminalCorrectionModel(double f__ghz, double h__meter,
     double w_s__meter, double R__meter, int clutter_type, double* A_h__db);
 
 // Private Functions
